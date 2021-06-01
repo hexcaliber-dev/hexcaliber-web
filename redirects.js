b5760db7f0bf293a7ca3a928b1788ec3521a07d9
Ben Cuan <github@bencuan.me>
2021-05-31 17:29:53 -0700

    Add game redirects

--- redirects.js
+++ redirects.js
@@ -3,6 +3,26 @@ const REDIRECTS = [
     old: "/drive",
     new: "https://drive.google.com/drive/u/0/folders/1Jpscfhg0XDrXZgClblQkPHSeOZRkNoTy",
   },
+  {
+    old: "/bulletsanctum",
+    new: "https://64bitpandas.itch.io/bullet-sanctum",
+  },
+  {
+    old: "/eggmageddon",
+    new: "https://epicrider.itch.io/eggmageddon",
+  },
+  {
+    old: "/bulletheaven",
+    new: "https://epicrider.itch.io/bullet-heaven",
+  },
+  {
+    old: "/convergence",
+    new: "https://xkindredkinesis.itch.io/convergence",
+  },
+  {
+    old: "/itsrudetopoint",
+    new: "https://64bitpandas.itch.io/its-rude-to-point",
+  },
 ];
 
 // Map to createRedirect (no need to modify anything below if creating new redirects)
