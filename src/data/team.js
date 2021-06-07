90d4f75a1993839e6d530023b1e0c3c6cba244b3
Epicrider <omarepic1@gmail.com>
2021-06-06 21:15:34 -0700

    Add Omar Profile in About

--- src/data/team.js
+++ src/data/team.js
@@ -23,6 +23,19 @@ const TEAM = [
              Bullet Sanctum, C:\\ONVERGENCE, and Eggmageddon. I also made this website (was fun)! \
              Outside of Hexcaliber, I'm a CS/Psych major at UC Berkeley who enjoys getting nuked by Gandhi while trying to build camel archers.",
 	},
+	{
+		name: "Omar Hossain (Epicrider/Static Savior)",
+		photo: "omarhossain.jpg",
+		links: {
+			website: "https://omarhossain.me",
+			github: "https://github.com/Epicrider",
+		},
+		favGames: "Zelda: Skyward Sword (don't @ me), Hollow Knight, God of War (2018), Ori and the Will of the Wisps",
+		bio: "How u doin'? I'm Omar (Epicrider/Static Savior)! I'm a programmer, game designer, and artist for Hexcaliber. \
+		I helped program and design all of our games and even wrote the story for C:\\ONVERGENCE. \
+		If I'm not making or playing games, you can usually find me drawing or unsucessfully trying to get fit. \
+		I'm majoring in CS at UC Irvine where I'm the only student refusing to go ZOT ZOT every 5 minutes.",
+	}
 ];
 
 export default TEAM;
