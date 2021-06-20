5ac0a51545d72a1e9d7fccea565d23ff07fd5b1e
Kevin Chu <kmhchu2001@gmail.com>
2021-06-20 13:13:27 -0700

    Update team.js to include my bio

--- src/data/team.js
+++ src/data/team.js
@@ -35,6 +35,20 @@ const TEAM = [
 		I helped program and design all of our games and even wrote the story for C:\\ONVERGENCE. \
 		If I'm not making or playing games, you can usually find me drawing or unsucessfully trying to get fit. \
 		I'm majoring in CS at UC Irvine where I'm the only student refusing to go ZOT ZOT every 5 minutes.",
+	},
+	{
+		name: "Kevin Chu (xKindredKinesis/XenSigilMedia)",
+		photo: "Xen Sigil Media - Icon.png",
+		links: {
+			website: "https://xkindredkinesis.itch.io/",
+			github: "https://github.com/KindredKinesis",
+		},
+		favGames: "DOOM Eternal, Devil May Cry 5, Sonic Mania, ULTRAKILL, Katana ZERO, Dead Cells, Shovel Knight: Specter of Torment",
+		bio: "Ni hao-dy! My name's Kevin. I'm a 2D artist, UI designer, and game designer for Hexcaliber. \
+		If there's an art asset from one of our games that you liked or hated, it was probably made by me. \
+		I also helped design the gameplay loops for Bullet Heaven, It's Rude To Point, and Bullet Sanctum. \
+		Besides game development, my hobbies include graphic design, writing stories, and simping for VTubers. \
+		Outside of Hexcaliber, I'm a CS major at San Jose State University, and I don't go outside nearly enough.",
 	}
 ];
 
